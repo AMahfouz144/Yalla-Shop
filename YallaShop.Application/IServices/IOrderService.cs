4813a279dcf588fdf8bb301955cfa8e00d94f5f4
Adham Mohamed <adhammohamed09900@gmail.com>
2026-04-19 21:05:16 +0200

    completed order feature

--- YallaShop.Application/IServices/IOrderService.cs
+++ YallaShop.Application/IServices/IOrderService.cs
@@ -11,6 +11,8 @@ namespace YallaShop.Application.IServices
     public interface IOrderService
     {
         Task<ResponseModel<IEnumerable<OrderResponseDto>>> GetAllOrdersAsync(string userId);
+        Task<ResponseModel<IEnumerable<OrderResponseDto>>> GetOrdersByCustomerAsync(string customerId);
+        Task<ResponseModel<IEnumerable<OrderResponseDto>>> GetOrdersBySellerAsync(string sellerId);
         Task<ResponseModel<OrderResponseDto>> GetOrderByIdAsync(int orderId);
         Task<ResponseModel<OrderStatus>> GetOrderStatusAsync(int orderId);
         Task<ResponseModel<bool>> UpdateOrderStatusAsync(int orderId, OrderStatus status);
